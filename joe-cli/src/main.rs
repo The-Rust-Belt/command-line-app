e111251d5d15dafb728100892cfce7eeaca7832a
jpcaulfi <joecaulfield29@yahoo.com>
2022-02-06 15:36:39 -0500

    CLI project from scratch

--- joe-cli/src/main.rs
+++ joe-cli/src/main.rs
@@ -1,5 +1,5 @@
 
-use std::io;
+use std::{io, thread, time};
 
 // --------------------------------------------------------
 
@@ -37,47 +37,58 @@ fn json() {
 fn launch() {
 
     loop {
+
+        println!("Enter command:");
+
         let mut command = String::new();
         io::stdin().read_line(&mut command);
 
-        match command.as_str() {
+        match command.trim() {
             "random" => {
                 println!("Launching Random Message Generator...");
+                thread::sleep(time::Duration::from_secs(2));
                 random_message();
                 break;
             },
             "countdown" => {
                 println!("Launching Count Down...");
+                thread::sleep(time::Duration::from_secs(2));
                 countdown();
                 break;
             },
             "login" => {
                 println!("Launching Login...");
+                thread::sleep(time::Duration::from_secs(2));
                 login();
                 break;
             },
             "snake" => {
                 println!("Launching Snake...");
+                thread::sleep(time::Duration::from_secs(2));
                 snake();
                 break;
             },
             "linkedlist" => {
                 println!("Launching Linked List...");
+                thread::sleep(time::Duration::from_secs(2));
                 linked_list();
                 break;
             },
             "race" => {
                 println!("Launching Race...");
+                thread::sleep(time::Duration::from_secs(2));
                 race();
                 break;
             },
             "json" => {
                 println!("Launching JSON...");
+                thread::sleep(time::Duration::from_secs(2));
                 json();
                 break;
             },
             _ => {
-                println!("Not Command. Try Again");
+                println!("Not a valid command. Try again.");
+                thread::sleep(time::Duration::from_secs(2));
             }
         }
     }
